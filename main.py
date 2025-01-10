e0e6cc5c18f34f02f434844959e2c9e02231edb8
Alexandr Semenets <alexandrsemenec01@gmail.com>
2025-01-10 23:20:29 +0200

    git init

---
+++ main.py
@@ -0,0 +1,14 @@
+# print
+print('wow')
+
+# main types
+print('hello world')
+
+print(123 + 321)
+
+print(True)
+
+print([1, 2, 'three'])
+print(2, 45, 5, True)
+
+print({'greet': 'hello', 'number': 23})
