7a9fced222edf3ccfa298b7816e28d742080843a
Alexandr Semenets <alexandrsemenec01@gmail.com>
2025-01-12 15:12:45 +0200

    [function]

--- main.py
+++ main.py
@@ -1,14 +1,61 @@
 # print
-print('wow')
-
-# main types
-print('hello world')
-
+print("wow")
+#
+# # main types
+print("hello world")
+#
 print(123 + 321)
-
+#
 print(True)
-
-print([1, 2, 'three'])
+#
+print([1, 2, "three"])
 print(2, 45, 5, True)
+#
+print({"greet": "hello", "number": 23})
 
-print({'greet': 'hello', 'number': 23})
+
+# functions
+def toSum(a, b):
+    summary = a + b
+    print(summary)
+    return summary
+
+
+toSum(1, 1)
+
+
+def greet(name):
+    print(f"Hello {name}")
+
+
+greet("alex")
+
+print(__name__)
+
+
+toType = type(12)
+print(toType)
+
+listExample = [2, 3, "maria"]
+print(listExample)
+
+distExample = {"name": "elena", "age": 45}
+print(distExample)
+
+print(type(distExample))
+
+exampleId = 1
+print(id(exampleId))
+
+print(len(listExample))
+
+listOfNumbers = [1, 3, 5]
+
+print(sum(listOfNumbers))
+
+print(dir())
+
+userInput = input("pls enter your name")
+
+
+print(userInput)
