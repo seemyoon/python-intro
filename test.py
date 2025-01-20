c47eed912c96f55d0bb684d13557725e8c638d05
Alexandr Semenets <alexandrsemenec01@gmail.com>
2025-01-20 15:59:51 +0200

    [f-stings], [method format], [strings], [strings method]

--- test.py
+++ test.py
@@ -97,8 +97,8 @@ print(id(findId))
 
 print(id(findId) == id(findId2))
 
-variableString = "jkhdfs"
-print(type(variableString))
+# variableString = "jkhdfs"
+# print(type(variableString))
 
 print(type(object))
 
@@ -107,3 +107,87 @@ inst2 = isinstance("sasha", int)
 
 print(inst)
 print(inst2)
+
+variableString = """Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."""
+print(variableString)
+
+
+# Built-in func in stings (3)
+exString = "Lorem Ipsum is simply dummy text of the printing"
+print(exString[7:10])  # range
+
+exString2 = "cvb"
+print(len(exString2))
+
+exString3 = "iuy"
+print(exString3[2])
+
+
+# prevent error of quotes
+preventExString = "It's my life"
+print(preventExString)
+# preventExString2 = 'It\'s my life'
+
+# methods stings
+# upper()
+trainForMethod = preventExString.upper()
+print(trainForMethod)
+# replace()/simply instance and with similar variables/
+preventExString.replace("my", "your")
+print(preventExString)
+
+trainForMethod2 = preventExString.replace("my", "your")
+print(trainForMethod2)
+# count()
+trainForMethod3 = preventExString.count(" ")
+print(trainForMethod3)
+
+# index()
+trainForMethod4 = preventExString.index("m")
+print(trainForMethod4)
+
+# capitalize()
+trainForMethod5 = preventExString.capitalize()
+print(trainForMethod5)
+
+# lower()
+trainForMethod6 = preventExString.lower()
+print(trainForMethod6)
+
+# find() /if we have value and no/
+trainForMethod7a = preventExString.find("my")
+print(trainForMethod7a)
+
+trainForMethod7b = preventExString.find("key")
+print(trainForMethod7b)
+
+# split()
+trainForMethod8 = preventExString.split(" ")
+print(trainForMethod8)
+
+# String Concatenation
+# Standard concatenation /simply ex and if we pass not only string, but integer as well:/
+exStringConcat = "Lorem Ipsum is" + "simply dummy text"
+print(exStringConcat)
+
+exStringConcat2 = "Lorem Ipsum is" + "simply dummy text" + str(32)
+print(exStringConcat2)
+
+# f-stings
+firstPartOfSent = "Lorem Ipsum is"
+secondPartOfSent = "simply dummy text"
+someNum = 2134
+
+exStringConcat3 = f"{firstPartOfSent}  {secondPartOfSent} {someNum}"
+print(exStringConcat3)
+
+# method format()
+name = "Sasha"
+town = "Kharkiv"
+
+exStringConcat3 = "hello, i'm {} and i from {}".format(name, town)
+print(exStringConcat3)
+
+# s%
+exStringConcat4 = "hello, i'm %s and i from %s" % (name, town)
+print(exStringConcat4)
