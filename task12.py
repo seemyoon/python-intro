36336148f71eef753c66813b9e97fc6574d9f561
Alexandr Semenets <alexandrsemenec01@gmail.com>
2025-01-31 23:13:46 +0200

    task 12 was done // part 2

--- task12.py
+++ task12.py
@@ -4,30 +4,30 @@
 # 3. If the value of the key is an integer, it must be multiplied by 2 before adding it to the tuple
 
 
-# def dict_to_list(my_dictionary: dict):
-#     new_list = []
-#     for k, v in my_dictionary.items():
-#         if isinstance(v, int):
-#             v = v * 2
-#         new_list.append((k, v))
-#     return new_list
-#
-#
-# new_dict = {
-#     "name": "Oleksandr",
-#     "age": 24,
-#     "speciality": {
-#         "speciality1": "Machine learning",
-#         "speciality2": "Data science",
-#         "speciality3": "Wev development",
-#         "speciality4": "Backend development",
-#         "speciality5": "Frontend development",
-#         "speciality6": "DevOps",
-#     },
-#     "hometown": "Kharkiv",
-# }
-# result = dict_to_list(new_dict)
-# print(result)
+def dict_to_list(my_dictionary: dict):
+    new_list = []
+    for k, v in my_dictionary.items():
+        if isinstance(v, int):
+            v = v * 2
+        new_list.append((k, v))
+    return new_list
+
+
+new_dict = {
+    "name": "Oleksandr",
+    "age": 24,
+    "speciality": {
+        "speciality1": "Machine learning",
+        "speciality2": "Data science",
+        "speciality3": "Wev development",
+        "speciality4": "Backend development",
+        "speciality5": "Frontend development",
+        "speciality6": "DevOps",
+    },
+    "hometown": "Kharkiv",
+}
+result = dict_to_list(new_dict)
+print(result)
 
 # Task 2
 # 1. Create a filter_list function that will filter the list
