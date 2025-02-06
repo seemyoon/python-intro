5e46a6b7fe90c702a8da94c5cef7693e8e9daaa1
Alexandr Semenets <alexandrsemenec01@gmail.com>
2025-02-06 02:42:41 +0200

    task 16 was done // task 2

--- task16.py
+++ task16.py
@@ -1,3 +1,4 @@
+# Task 1
 # Create another instance of the Post class and call 'like' method for it
 # Check 'likes_qty' for both instances
 
@@ -22,3 +23,30 @@ my_post2.like()
 
 print(my_post.likes_qty)
 print(my_post2.likes_qty)
+
+# Task 2
+# Create class calculator with the following operations: add, subtract, divide, mult. Methods must be static
+
+
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def divide(a, b):
+        return a / b
+
+    @staticmethod
+    def mult(a, b):
+        return a * b
+
+
+print(Calculator.add(30, 5))
+print(Calculator.subtract(30, 10))
+print(Calculator.divide(30, 2))
+print(Calculator.mult(40, 10))
